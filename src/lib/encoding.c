7042342cb2a2747460d4fd5edb023e51a61e27e2
Chris Hundt <chris@robocourt.com>
2015-07-07 17:48:49 -0400

    Base64 encoding

--- src/lib/encoding.c
+++ src/lib/encoding.c
@@ -9,9 +9,67 @@ char hex_digits[16] = {
 	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
 };
 
+/*
+	The byte's two high bits will be ignored.
+*/
+char byte_to_base64_digit(byte b) {
+	b = b & 0x3f;
+	if (b < 26) return 'A' + b;
+	if (b < 52) return 'a' + (b - 26);
+	if (b < 62) return '0' + (b - 52);
+	return b == 62 ? '=' : '/';
+}
+
+int num_base64_digits(size_t num_bytes) {
+	return ((num_bytes + 2) / 3) * 4;
+}
+
+int num_hex_digits(size_t num_bytes) {
+	return num_bytes * 2;
+}
+
+int num_bytes_from_hex(size_t num_hex_digits) {
+	return (num_hex_digits + 1) / 2;
+}
+
+int bytes_to_base64(byte *bytes, size_t num_bytes, char *buf, size_t buf_size) {
+	size_t num_chars = num_base64_digits(num_bytes);  // 4 base64 chars fit in 3 bytes
+	if (num_chars + 1 > buf_size) {
+		return -1;
+	}
+	buf[num_chars] = '\0';
+	for (int i = 0; i < num_bytes; i+= 3) {
+		uint32_t group;  // the 24-bit group of bits we are handling now
+		// Read in from input
+		group = bytes[i] << 16;
+		if (i + 1 < num_bytes) {
+			group |= (bytes[i + 1] << 8);
+		}
+		if (i + 2 < num_bytes) {
+			group |= bytes[i + 2];
+		}
+		// Write out to output
+		if (i + 2 < num_bytes) {
+			buf[i / 3 * 4 + 3] = byte_to_base64_digit(group & 0x3f);
+		} else { // missing one or more bytes
+			buf[i / 3 * 4 + 3] = '=';
+		}
+		group = group >> 6;
+		if (i + 1 < num_bytes) {
+			buf[i / 3 * 4 + 2] = byte_to_base64_digit(group & 0x3f);
+		} else {  // missing two bytes
+			buf[i / 3 * 4 + 2] = '=';
+		}
+		group = group >> 6;
+		buf[i / 3 * 4 + 1] = byte_to_base64_digit(group & 0x3f);
+		group = group >> 6;
+		buf[i / 3 * 4] = byte_to_base64_digit(group & 0x3f);
+	}
+	return num_chars;
+}
 
 int bytes_to_hex(byte *bytes, size_t num_bytes, char *buf, size_t buf_size) {
-	size_t num_chars = num_bytes * 2;
+	size_t num_chars = num_hex_digits(num_bytes);
 	if (num_chars + 1 > buf_size) {
 		return -1;
 	}
@@ -26,7 +84,7 @@ int bytes_to_hex(byte *bytes, size_t num_bytes, char *buf, size_t buf_size) {
 
 int hex_to_bytes(char* hex, byte *buf, size_t buf_size) {
 	size_t num_chars = strlen(hex);
-	size_t num_bytes = (strlen(hex) + 1) / 2;
+	size_t num_bytes = num_bytes_from_hex(num_chars);
 	if (num_bytes > buf_size) {
 		return -1;
 	}
--- src/set1/q1.c
+++ src/set1/q1.c
@@ -4,17 +4,15 @@
 
 int main() {
 	char* before = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-	//char* before = "ff";
-	size_t max_bytes = (strlen(before) + 1)/2;
+	size_t max_bytes = num_bytes_from_hex(strlen(before));
 	byte* as_bytes = calloc(max_bytes, sizeof(byte));
 	size_t num_bytes = hex_to_bytes(before, as_bytes, max_bytes);
-	char* after = calloc(num_bytes * 2 + 1, sizeof(char));
-	bytes_to_hex(as_bytes, num_bytes, after, num_bytes * 2 + 1);
+	char* as_base64 = calloc(num_base64_digits(num_bytes) + 1, sizeof(char));
+	bytes_to_base64(as_bytes, num_bytes, as_base64, num_base64_digits(num_bytes) + 1);
 
-    printf("Before: %s\n", before);
-    print_bytes(as_bytes, num_bytes);
-    printf("After: %s\n", after);
+    printf("Hex: %s\n", before);
+    printf("Base64: %s\n", as_base64);
 
     free(as_bytes);
-    free(after);
+    free(as_base64);
 }
