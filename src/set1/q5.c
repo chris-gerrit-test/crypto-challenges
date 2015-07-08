d4f912c2736090c68d9bba4ff39d290db476a3c7
Chris Hundt <chris@robocourt.com>
2015-07-08 17:29:40 -0400

    Q5

---
+++ src/set1/q5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include "encoding.c"
+#include "math.c"
+
+int main() {
+	char msg[] = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+	char *key = "ICE";
+	repeated_xor((byte*)msg, strlen(msg), (byte*)key, strlen(key), (byte*)msg);
+	size_t hex_digits = num_hex_digits(strlen(msg));
+	char* buf = calloc(hex_digits + 1, sizeof(char));
+	bytes_to_hex((byte*)msg, strlen(msg), buf, hex_digits + 1);
+    printf("%s\n", buf);
+
+    free(buf);
+}
