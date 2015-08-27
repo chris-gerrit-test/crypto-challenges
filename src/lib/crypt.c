2a7855c33a2f2d7a46f2ee8d0624b6a08b9125cf
Chris Hundt <chris@robocourt.com>
2015-08-27 19:59:11 -0400

    Fix off-by-one bounds check

--- src/lib/crypt.c
+++ src/lib/crypt.c
@@ -15,7 +15,7 @@ int pkcs7(char *buf, size_t buf_size, size_t block_size) {
 	for (size_t i = 0; i < buf_size; ++i) {
 		if (buf[i] == '\0') {
 			size_t padding_size = block_size - (i % block_size);
-			if (i + padding_size > buf_size) {
+			if (i + padding_size >= buf_size) {
 				return -1;
 			}
 			for (size_t j = 0; j < padding_size; ++j) {
