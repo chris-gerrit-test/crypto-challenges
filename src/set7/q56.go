2457da579c09fbc07d4cec030e329e7c7f06792e
Chris Hundt <chris@robocourt.com>
2015-12-07 18:21:13 -0500

    Q56 finished!

--- src/set7/q56.go
+++ src/set7/q56.go
@@ -1,74 +1,82 @@
 package main
 
 import (
-	"bufio"
+	"crypto/rand"
 	"crypto/rc4"
-	"fmt"
-	"os"
+	"encoding/base64"
+	"log"
+	"strings"
 )
 
-type Count struct {
-	b     byte
-	count uint64
-}
-
-type Counts []Count
-
-func (c Counts) Len() int {
-	return len(c)
-}
-
-func (c Counts) Less(i, j int) bool {
-	return c[i].count < c[j].count
-}
-
-func (c Counts) Swap(i, j int) {
-	z := c[i]
-	c[i] = c[j]
-	c[j] = z
+type Peephole struct {
+	request          []byte
+	counts1, counts2 map[byte]int
 }
 
 func main() {
-	counts := make([]map[byte]uint64, 32)
+	counts := make([]map[byte]int, 32)
 	for i, _ := range counts {
-		counts[i] = make(map[byte]uint64, 0)
+		counts[i] = make(map[byte]int, 0)
 	}
-	secret := "YELLOW SUBMARINEYELLOW SUBMARINE"
-	s := make([]byte, len(secret))
+	secret, err := base64.StdEncoding.DecodeString("QkUgU1VSRSBUTyBEUklOSyBZT1VSIE9WQUxUSU5F")
+	if err != nil || len(secret) != 30 {
+		panic(err)
+	}
+	peepholes := make(map[int]*Peephole)
+	for off := -1; off < 15; off++ {
+		peepholes[off] = &Peephole{
+			request: []byte("/" + strings.Repeat("x", 14-off) + string(secret)),
+			counts1: make(map[byte]int, 0),
+			counts2: make(map[byte]int, 0),
+		}
+	}
 	keySize := 8
 	key := make([]byte, keySize)
 
-	f, err := os.Open("random.dat")
-	if err != nil {
-		panic(err)
-	}
-	r := bufio.NewReaderSize(f, 16384)
-	var off int64 = 0
-	for i := 0; i < 100000000; i++ {
-		copy(s, secret)
-		//n, err := rand.Read(key)
-		n, err := r.Read(key)
-		if n != keySize || err != nil {
-			panic(err)
+	s := make([]byte, len(secret)+16)
+	for trial := 0; trial < 10000000; trial++ {
+		for idx, p := range peepholes {
+			n, err := rand.Read(key)
+			if n != keySize || err != nil {
+				panic(err)
+			}
+			c, err := rc4.NewCipher(key)
+			if err != nil {
+				panic(err)
+			}
+			c.XORKeyStream(s, p.request)
+			p.counts1[s[15]^240] += 1
+			if idx < 15 {
+				p.counts2[s[31]^224] += 1
+			}
 		}
-		off += int64(keySize)
-		c, err := rc4.NewCipher(key)
-		if err != nil {
-			panic(err)
-		}
-		c.XORKeyStream(s, s)
-		for b := 0; b < len(counts); b++ {
-			counts[b][s[b]^secret[b]] += 1
-		}
-	}
 
-	// for c, count := range counts {
-	// 	log.Printf("%3d: %d", c^byte(240), count)
-	// }
-
-	for b := 0; b < len(counts); b++ {
-		for c := 0; c < 256; c++ {
-			fmt.Printf("C%02d %3d %d\n", b, c, counts[b][byte(c)])
+		if trial%100000 == 99999 {
+			// Show our best guess so far
+			for idx, p := range peepholes {
+				if idx >= 0 {
+					s[idx] = '?'
+					max := 0
+					for b, count := range p.counts1 {
+						// Assume ASCII without any funny whitespace
+						if b >= 32 && b <= 126 && count > max {
+							s[idx] = b
+							max = count
+						}
+					}
+				}
+				if idx < 15 {
+					s[idx+16] = '?'
+					max := 0
+					for b, count := range p.counts2 {
+						if b >= 32 && b <= 126 && count > max {
+							s[idx+16] = b
+							max = count
+						}
+					}
+				}
+			}
+			log.Printf("Best guess after %8d trials: %s", trial+1, s[:30])
 		}
 	}
 }
